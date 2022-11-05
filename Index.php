240c26bf125a39d85fd878154e3c8367d328160c
TeteuTeixeira <mateus.programacao29@gmail.com>
2022-11-05 14:54:44 -0300

    Alterações no css

--- Index.php
+++ Index.php
@@ -27,11 +27,11 @@ if(isset($_POST['submit']))
             <form method="POST" action="processa.php">
                 <label for="nome">Nome</label><br>
                 <i class="fa-solid fa-user"></i>
-                <input type="text" name="nome" placeholder="Digite seu nome completo" style="width: 285px;"><br><br>
+                <input type="text" id="nome" name="nome" placeholder="Digite seu nome completo" style="width: 325px;"><br><br>
 
                 <label for="ano">Ano</label><br>
                 <i class="fa-solid fa-book"></i>
-                <input type="text" name="ano" placeholder="Digite seu ano" style="width: 300px;"><br><br>
+                <input type="text" id="ano" name="ano" placeholder="Digite seu ano" style="width: 325px;"><br><br>
 
 
                 <p>Genero</p>
@@ -40,7 +40,7 @@ if(isset($_POST['submit']))
                 <input type="radio" id="feminino" name="genero" value="feminino">
                 <label for="feminino" class="radio">Feminino</label>
                 <input type="radio" id="outro" name="genero" value="outro"> 
-                <label for="outros" class="radio">Outros</label><br><br>
+                <label for="outro" class="radio">Outros</label><br><br>
 
                 <label for="texto"></label>
                 <textarea name="texto" id="texto" cols="36" rows="10" placeholder="Digite como foram as suas férias"></textarea><br><br>
