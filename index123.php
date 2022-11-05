848135b43aa468045bd01af2ed1b4d5fe7db0980
TeteuzinTeixeira <mateus.programcao29@gmail.com>
2022-11-04 23:16:37 -0300

    Meu primeiro commit

---
+++ index123.php
@@ -0,0 +1,53 @@
+<?php 
+if(isset($_POST['submit']))
+{
+    echo($_POST['nome']);
+    print_r($_POST['ano']);
+    print_r($_POST['texto']);
+    print_r($_POST['genero']);
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style123.css">
+   
+</head>
+<body>
+    <div class="container">
+        <div class="imagem">
+            <img class="crianca"src="Imagens/criancas_brincando.png" alt="imagem">
+        </div>
+
+        <div class="containerForm">
+            <form method="POST" action="processa.php">
+                <label for="nome">Nome: </label>
+                <input type="text" name="nome" placeholder="Digite seu nome completo" style="width: 285px;"><br><br>
+
+                <label for="ano">Ano: </label>
+                <input type="text" name="ano" placeholder="Digite seu ano" style="width: 300px;"><br><br>
+
+
+                <p>Genero: </p><br>
+                <input type="radio" id="masculino" name="genero" value="masculino">
+                <label for="masculino">Masculino</label>
+                <input type="radio" id="feminino" name="genero" value="feminino">
+                <label for="feminino">Feminino</label>
+                <input type="radio" id="outro" name="genero" value="outro">
+                <label for="outro">Outros</label><br><br>
+
+                <label for="texto"></label>
+                <textarea name="texto" id="texto" cols="36" rows="10" placeholder="Digite como foram as suas férias"></textarea><br><br>
+                <div class="box_button">
+                    <input type="submit" value="Cadastrar" class="botao">
+                </div>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ processa.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once("conexao.php");
+
+$nome = ($_POST ['nome']);
+$ano = ($_POST ['ano']);
+$genero = ($_POST ['genero']);
+$texto = ($_POST ['texto']);
+
+$result_usuario = "INSERT INTO Usuarios (nome, ano,genero,texto) VALUES ('$nome','$ano','$genero', '$texto')";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+if(mysqli_insert_id($conn)){
+    header("Location:index123.php");
+}else{
+    header("Location:index123.php");
+}
+
+?>
