44b47c8f78ac4850a9c6034c30a311a7a2a8ce39
TeteuTeixeira <mateus.programacao29@gmail.com>
2022-11-05 14:29:46 -0300

    Inclusões de fontawsome

--- Index.php
+++ Index.php
@@ -16,7 +16,6 @@ if(isset($_POST['submit']))
     <title>Nos envie suas experiências</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-   
 </head>
 <body>
     <div class="container">
@@ -50,7 +49,6 @@ if(isset($_POST['submit']))
                 </div>
             </form>
         </div>
-    </div>
-    
+    </div>    
 </body>
 </html>
